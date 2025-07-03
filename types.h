64079ce1a1ef58c4ea2e9bb368f2b7eb7c040778
richardsonnick <nic.rr@icloud.com>
2025-07-03 14:07:35 -0400

    Add ip_packet_t

---
+++ types.h
@@ -0,0 +1,18 @@
+#include <stdint.h>
+// https://datatracker.ietf.org/doc/html/rfc791#section-3.1
+typedef struct {
+    uint8_t version;  // 4 bits
+    uint8_t ihl;  // 4 bits
+    uint8_t type_of_service; // 8 bits
+    uint16_t total_length; // 16 bits
+    uint16_t identification; // 16 bits
+    uint8_t flags; // 3 bits
+    uint16_t fragment_offset; // 13 bits
+    uint8_t time_to_live; // 8 bits
+    uint8_t protocol; // 8 bits
+    uint16_t header_checksum; // 16 bits
+    uint32_t source_address; // 32 bits
+    uint32_t destination_address; // 32 bits
+    // ...options
+    // ...padding
+} ip_packet_t;
