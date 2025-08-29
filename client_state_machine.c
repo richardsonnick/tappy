bfa1488ecdf7d1355ed3b64009db44a862388b5b
Your Name <you@example.com>
2025-08-29 18:57:44 +0000

    Handle SYN,ACK in SYN_SENT state and process received packet

--- client_state_machine.c
+++ client_state_machine.c
@@ -63,6 +63,7 @@ TCP_STATE client_handle_event(tcp_connection_t* conn, TCP_EVENT event, const tcp
                 return SYN_RECEIVED;
             } else if (event == RECEIVE && packet->flags == TCP_FLAG_SYN | TCP_FLAG_ACK) {
                 printf("Got SYN,ACK from SYN_SENT state!\n");
+                process_received_packet(conn->tcb, tcp_ip->tcp_packet);
                 simple_send_flag(conn, TCP_FLAG_ACK);
                 printf("Sent ACK to complete handshake\n");
                 return ESTABLISHED;
