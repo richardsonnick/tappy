cdecffbb008be5e2328afd7eaa34a1ed46bb65b3
richardsonnick <nic.rr@icloud.com>
2025-08-29 19:43:48 +0000

    Remove useless func

--- tcp.h
+++ tcp.h
@@ -10,13 +10,6 @@ tcp_connection_t* init_tcp_stack(ip_addr_t* source_ip, ip_addr_t* destination_ip
 
 tcp_ip_t* make_packet(const tcb_t* tcb, const uint8_t flags, const uint8_t* data, const size_t data_len);
 
-uint8_t* data_to_tcp_buf(const uint8_t* data, const size_t data_len, 
-    const ip_addr_t* source_ip,
-    const ip_addr_t* destination_ip,
-    const uint16_t source_port,
-    const uint16_t destination_port,
-    size_t* out_total_packet_len);
-
 uint16_t compute_ip_checksum(const ip_header_t* header);
 uint16_t compute_tcp_packet_checksum(const tcp_packet_t* packet,
                                     uint32_t src_ip, uint32_t dst_ip,
